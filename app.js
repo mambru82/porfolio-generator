6a69afb3ed113ea11d9086985e5de27aba327c79
mambru <ecastro82@gmail.com>
2021-02-02 20:56:18 -0600

    initial argv statement

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i< profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }   
+//     profileDataArr.forEach((profileItem) => {console.log(profileItem)});
+// }
+
+
+// printProfileData(profileDataArgs);
+// var three = 'three: declared outside the block';
+
+// if(true===true) {
+//     three = 'three: changed inside the block';
+//     console.log(three);
+// }
+
+// console.log(three);
+
+// let four = 'four: outside the block';
+
+// if(true===true) {
+//     four = 'four: inside the block';
+//     console.log(four);
+// }
+
+// console.log(four);

5f9f9f2d2612383174a0d291c9706ddcc4f6a566
mambru <ecastro82@gmail.com>
2021-02-05 22:02:39 -0600

    write file function using promises

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js')
-
+const { writeFile, copyFile } = require('./utils/generate-site.js')
 
 
 const promptUser = () => {
@@ -132,15 +131,36 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML= generatePage(portfolioData);
-
-
-        fs.writeFile('./index.html', pageHTML, err => {
-             if (err) throw err;
-
-             console.log('Portfolio complete! Check out index.html to see the output!');
-         });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+
+        // fs.writeFile('./dist/index.html', pageHTML, err => {
+        //      if (err) throw err;
+
+        //      console.log('Portfolio complete! Check out index.html to see the output!');
+        //      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        //          if(err) {
+        //              console.log(err);
+        //              return;
+        //          }
+        //          console.log('Style sheet copied successfully!');
+        //      })
+        //  });
+    // });
 
 
 

7f0c79668d9aba7ea27bdbcdd49a396466682363
mambru <ecastro82@gmail.com>
2021-02-05 21:21:38 -0600

    generate projects on HTML

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -128,19 +132,17 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML= generatePage(portfolioData);
+
+
+        fs.writeFile('./index.html', pageHTML, err => {
+             if (err) throw err;
+
+             console.log('Portfolio complete! Check out index.html to see the output!');
+         });
     });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
-
-// const pageHTML= generatePage(name, github);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
 // const printProfileData = profileDataArr => {
